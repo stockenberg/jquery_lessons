3a688da9b509bb11475a6e2ba286a13d38e5ee6c
Marten Stockenberg <MStockenberg@googlemail.com>
2017-01-31 11:52:52 +0100

    effects comments and events comments

---
+++ js/effects.js
@@ -0,0 +1,20 @@
+/**
+ * Effects:
+ * $("").hide();
+ * $("").show();
+ * $("").toggle();
+ * $("").fadeIn();
+ * $("").fadeOut();
+ * $("").fadeToggle();
+ * $("").fadeTo();
+ * $("").slideDown();
+ * $("").slideUp();
+ * $("").slideToggle();
+ * $("").animate();
+ * $("").stop();
+*/
+
+
+
+
+
---
+++ js/events.js
@@ -0,0 +1,28 @@
+/**
+ * Created by workstation on 31.01.17.
+ */
+
+/**
+* $("").click();
+* $("").dblclick();
+* $("").mouseenter();
+* $("").mouseleave();
+* $("").mousedown();
+* $("").mouseup();
+* $("").hover();
+* $("").focus();
+* $("").blur();
+* $("").on();
+ *
+ * $("p").on({
+    mouseenter: function(){
+        $(this).css("background-color", "lightgray");
+    },
+    mouseleave: function(){
+        $(this).css("background-color", "lightblue");
+    },
+    click: function(){
+        $(this).css("background-color", "yellow");
+    }
+});
+ */
